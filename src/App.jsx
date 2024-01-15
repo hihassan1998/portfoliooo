b4507bebe3a17649151f8772d0d2e489c0044f29
hihassan1998 <hassanihussain1998@gmail.com>
2024-01-15 17:21:59 +0100

    2nd initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./app.scss"
 
 const App = () => {
   return <div>
-    <section>Hero</section>
+    <section><a href="">hello</a></section>
     <section>Paralax</section>
     <section>Services</section>
     <section>Parallax</section>
