698fd4dfd9937748c029006ad687df06a267fed3
hihassan1998 <hassanihussain1998@gmail.com>
2024-01-17 08:26:40 +0100

    Commiting Parallax pages DSGN animated half way

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import "./app.scss"
 import Navbar from "../components/navbar/NAvbar";
 import Hero from "../components/hero/Hero";
-
+import Parallax from "../components/parallax/Parallax";
 
 const App = () => {
   return <div>
@@ -10,9 +10,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Tjänster">Parallax</section>
+    <section id="Tjänster"> <Parallax type= "services"/> </section>
     <section>Services</section>
-    <section id="Portfölj">Parallax</section>
+    <section id="Portfölj"> <Parallax type= "portfolio"/> </section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
