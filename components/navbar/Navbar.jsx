5de4411ca8027a96d9a0beb3b7d323681ad34775
hihassan1998 <hassanihussain1998@gmail.com>
2024-01-21 00:59:05 +0100

    Tjänster page updated.

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         animate={{opacity:1, scale:1}}
         transition={{duration:2}}>
           <a href="https://www.facebook.com/hassan.idr1/" target="_blank" rel="noopener noreferrer"><FaFacebook /> </a>
-          <a href="https://www.linkedin.com/in/hassan-hussain-3b840429a" target="_blank" rel="noopener noreferrer"> <FaLinkedin /> </a>
+          <a href="https://www.linkedin.com/in/hassan-hussain-3b840429a" target="_blank" rel="noopener noreferrer"></a>  
           <a href="https://github.com/hihassan1998" target="_blank" rel="noopener noreferrer"> <FaGithub /> </a>
         </motion.div>
       </div>
--- components/services/Services.jsx
+++ components/services/Services.jsx
@@ -1,13 +1,63 @@
-import "./services.scss"
+import "./services.scss";
+import {FaCode, FaReact, FaServer, FaDatabase} from "react-icons/fa"
+import { SiExpress } from "react-icons/si";
+import { DiNodejsSmall } from 'react-icons/di';
+import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <div className="Services">
-        <div className="textContainer"></div>
-        <p>Frontend Utvecklare | Backend Utvecklare | Aspirerande Full Stack Utveckalare | Mitt focus är mot frontend men kan vara en del av ert fullstack </p>
-        <div className="titleContainer"></div>
-        <div className="listContainer"></div>
-    </div>
+    <motion.div className="Services">
+        <motion.div className="textContainer">
+        <p> Frontend Utvecklare | Backend Utvecklare | Mitt focus är mot frontend utveckling och MERN fullstack utveckling <FaDatabase/> <SiExpress/> <FaReact/> <DiNodejsSmall/> <br /> 
+        <FaCode/> Aspirerande Full Stack Utveckalare
+        </p> 
+        <hr />
+        </motion.div>
+        <motion.div className="titleContainer">
+            <div className="title">
+                <img src="/people.webp" alt="" />
+                <h1>
+                    Unika <br /> idéer
+                </h1>
+            </div>
+            <div className="title">
+            <h1>
+                For Your<br /> Business
+            </h1>
+            <button>WHAT I DO ?</button>
+        </div>
+        </motion.div>
+        <motion.div className="listContainer">
+            <div className="box" whileHover={{background:"lightgray", color:"black"}}>
+                <h2>Branding</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempoSed do eiusmod r incididunt ut labore et
+                </p> 
+                <button>Gå</button>              
+            </div>
+            <div className="box" whileHover={{background:"lightgray", color:"black"}}>
+                <h2>Branding</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+                </p> 
+                <button>Gå</button>              
+            </div>
+            <div className="box" whileHover={{background:"lightgray", color:"black"}}>
+                <h2>Branding</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+                </p> 
+                <button>Gå</button>              
+            </div>
+            <div className="box" whileHover={{background:"lightgray", color:"black"}}>
+                <h2>Branding</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+                </p> 
+                <button>Gå</button>              
+            </div>
+        </motion.div>
+    </motion.div>
   )
 }
 
