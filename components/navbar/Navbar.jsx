1da3f919a98fae9e9ebf276854a1472994fb9fdc
hihassan1998 <hassanihussain1998@gmail.com>
2024-02-05 18:18:34 +0100

    made minor changes to navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -21,9 +21,15 @@ const Navbar = () => {
         initial={{opacity:0, scale:0.5}}
         animate={{opacity:1, scale:1}}
         transition={{duration:2}}>
-          <a href="https://www.facebook.com/hassan.idr1/" target="_blank" rel="noopener noreferrer"><FaFacebook /> </a>
-          <a href="https://www.linkedin.com/in/hassan-hussain-3b840429a" target="_blank" rel="noopener noreferrer"></a>  
-          <a href="https://github.com/hihassan1998" target="_blank" rel="noopener noreferrer"> <FaGithub /> </a>
+          <a href="https://www.facebook.com/hassan.idr1/" target="_blank" rel="noopener noreferrer">
+            <FaFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/hassan-hussain-3b840429a" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin/>
+            </a>  
+          <a href="https://github.com/hihassan1998" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+            </a>
         </motion.div>
       </div>
     </div>
